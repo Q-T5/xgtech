251506f1b25ff92a1c7e8dd7d741a975c99deddf
Q-T5 <bikathimartin@outlook.com>
2023-01-03 10:58:44 +0300

    Map main routes to views

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,9 +1,38 @@
 import { createRouter, createWebHistory } from "vue-router"
+import HomeView from '/src/views/HomeView.vue'
+import GamesView from '/src/views/GamesView.vue'
+import ProductsView from '/src/views/ProductsView.vue'
+import ServicesView from '/src/views/ServicesView.vue'
+import ContactUsView from '/src/views/ContactUsView.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        {
+            path: "/",
+            component: HomeView,
+            name: "HomeView"
+        },
+        {
+            path: "/games",
+            component: GamesView,
+            name: "GamesView"
+        },
+        {
+            path: "/products",
+            component: ProductsView,
+            name: "ProductsView"
+        },
+        {
+            path: "/services",
+            component: ServicesView,
+            name: "ServicesView"
+        },
+        {
+            path: "/contact",
+            component: ContactUsView,
+            name: "ContactUsView"
+        }
     ]
 });
 
