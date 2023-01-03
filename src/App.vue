b3f9cc823d8bcc64409aee0a2b422fcf05aeda5d
Q-T5 <bikathimartin@outlook.com>
2023-01-03 12:34:24 +0300

    Mount main routes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
-  <div>
-    <h1 class="text-3xl underline">Hello World</h1>
-    <v-btn color="blue">Click Me</v-btn>
+  <div class="w-screen h-screen">
+    <nav class="h-[8%] py-2 px-4 flex justify-between items-center">
+      <div class="inline-flex space-x-1 items-center">
+        <img src="/src/assets/xgamer-logo.svg" alt="xgamer-logo" class="w-8" />
+        <h1 class="text-lg tracking-wide">XGAMERtechnologies</h1>
+      </div>
+      <div class="flex items-center h-full space-x-4">
+        <router-link 
+          v-for="(route, index) in mainRoutes" :key="index" :value="index"
+          class="main-route" :to="{ name: route.name }"
+          :class="$route.name === route.name ? 'active-route' : ''">
+          {{ route.text }}
+        </router-link>
+      </div>
+    </nav>
+    <router-view>
+
+    </router-view>
   </div>
 </template>
 
 <script lang="js">
+import { ref } from 'vue'
+
 export default {
   name: "App",
   setup() {
-    return {
+    // reactive data
+    const mainRoutes = ref([
+      { name: "HomeView", text: "Home" },
+      { name: "GamesView", text: "Games" },
+      { name: "ProductsView", text: "Products" },
+      { name: "ServicesView", text: "Services" },
+      { name: "ContactUsView", text: "Contact Us" },
+    ]);
 
+    return {
+      mainRoutes
     }
   }
 }
 </script>
+
+<style>
+  html {
+    overflow: hidden !important;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+  }
+
+  html::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+</style>
