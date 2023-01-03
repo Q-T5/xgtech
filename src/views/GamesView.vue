f5a0cd8351b9f7db348afe6470d9987d80b77abd
Q-T5 <bikathimartin@outlook.com>
2023-01-03 21:51:09 +0300

    Mount game display component

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -2,11 +2,19 @@
     <div>
         <h1>Games View page</h1>
     </div>
+    <div>
+        <game-display-comp />
+    </div>
 </template>
 
 <script lang="js">
+import GameDisplayComp from '../components/GameDisplayComp.vue'
+
 export default {
     name: "GamesView",
+    components: {
+        GameDisplayComp
+    },
     setup: function() {
         
     }
