f36dcc4e8b36636153af3e01234903dfc80c284f
Q-T5 <bikathimartin@outlook.com>
2023-01-04 00:09:39 +0300

    Mount GameExpandedComp component

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,22 +1,36 @@
 <template>
-    <div>
+    <div class="h-screen w-screen">
         <h1>Games View page</h1>
+        <div>
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+        </div>
     </div>
-    <div>
-        <game-display-comp />
-    </div>
+    <teleport to="#games-expanded">
+        <game-expanded-comp 
+            v-if="showExpandedView"
+            @closeExpandedView="showExpandedView = false" />
+    </teleport>
 </template>
 
 <script lang="js">
+import { ref } from 'vue'
 import GameDisplayComp from '../components/GameDisplayComp.vue'
+import GameExpandedComp from '../components/GameExpandedComp.vue'
 
 export default {
     name: "GamesView",
     components: {
-        GameDisplayComp
+        GameDisplayComp, GameExpandedComp
     },
     setup: function() {
+        // reactive data
+        const showExpandedView = ref(false);
         
+        // functions
+
+        return {
+            showExpandedView
+        }
     }
 }
 </script>
