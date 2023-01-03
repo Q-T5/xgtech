6a4584dd14d30a224dea7b1797cb5c4be2c96804
Q-T5 <bikathimartin@outlook.com>
2023-01-03 09:47:29 +0300

    Delete boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 class="text-3xl underline">Hello World</h1>
+    <v-btn color="blue">Click Me</v-btn>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <v-btn>Hello World</v-btn>
-  <h1 class="text-3xl underline">Test Hello World</h1>
 </template>
+
+<script lang="js">
+export default {
+  name: "App",
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
