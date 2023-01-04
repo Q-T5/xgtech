2566052ed266e6d09b8693985c266224324514a9
Q-T5 <bikathimartin@outlook.com>
2023-01-04 11:07:52 +0300

    Update look of view component

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,8 +1,52 @@
 <template>
-    <div class="h-screen w-screen">
-        <h1>Games View page</h1>
-        <div>
+    <div class="h-screen">
+        <div class="h-[7%] flex items-center justify-between px-2">
+            <div class="flex space-x-2 items-center relative">
+                <h1 class="text-3xl font-ubuntu">Games</h1>
+                <input 
+                    type="text" 
+                    class="game-search-box"
+                    placeholder="Search Game"
+                    maxlength="35" />
+                <div class="absolute right-0.5 top-1 w-fit h-fit">
+                    <v-btn 
+                        color="purple"
+                        variant="flat"
+                        rounded="pill"
+                        size="small">search</v-btn>
+                </div>
+            </div>
+            <div class="pr-4">
+                <v-btn 
+                    icon 
+                    color="purple"
+                    size="small">
+                    <v-icon>mdi-filter-menu</v-icon>
+                </v-btn>
+            </div>
+        </div>
+        <div class="h-[93%] max-h-[95%] overflow-scroll grid grid-cols-6 px-2">
             <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <game-display-comp @showExpandedView="showExpandedView = true" />
+            <div class="flex items-center justify-center">
+                <v-btn
+                    rounded="pill"
+                    color="purple"
+                    variant="flat"
+                    append-icon="mdi-monitor-arrow-down-variant">load more</v-btn>
+            </div>
         </div>
     </div>
     <teleport to="#games-expanded">
