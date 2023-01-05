fd734b29425da5cfe78cf3b51fceeeb442a6dd6b
Q-T5 <bikathimartin@outlook.com>
2023-01-05 22:46:44 +0300

    Add cart view route

---
+++ src/views/CartView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-red-500 h-screen">
+        <v-btn 
+            icon 
+            color="purple" 
+            @click="$router.go(-1)"
+            variant="flat">
+            <v-icon>mdi-arrow-left-thin</v-icon>
+      </v-btn>
+    </div>
+</template>
+
+<script lang="js">
+export default {
+    name: "CartView",
+    setup: function() {
+
+    }
+}
+</script>
