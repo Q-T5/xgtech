d3728c44f206f9cf494eef587bbcb3a07f4cf2e7
Q-T5 <bikathimartin@outlook.com>
2023-01-06 11:02:06 +0300

    Add under-construction banner

--- src/views/subviews/TradeInsView.vue
+++ src/views/subviews/TradeInsView.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        <h1>Tradeins view</h1>
+    <div class="h-screen flex flex-col items-center justify-center">
+        <img 
+            src="/src/assets/under-construction.svg" 
+            alt="under-construction"
+            class="w-28" />
+        <h1 class="font-ubuntu text-5xl">Page Under Construction</h1>
     </div>
 </template>
 
