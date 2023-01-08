76da418eee2b253f48dc1e5e7ef0e82ff8f38b58
Q-T5 <bikathimartin@outlook.com>
2023-01-08 11:30:50 +0300

    Add new color schemes

--- src/main.js
+++ src/main.js
@@ -18,6 +18,35 @@ import router from './utils/router'
 // setup pinia
 const pinia = createPinia();
 
+// custom themes
+const customDarkTheme = {
+    dark: false,
+    colors: {
+        background: "#31393F",
+        surface: "#31393F",
+        primary: "#a00b35",
+        secondary: "#92f986",
+        error: "#E33544",
+        info: "#3D85D6",
+        success: "#61EAC8",
+        warning: "#F2C26E"
+    }
+}
+
+const customLightTheme = {
+    dark: false,
+    colors: {
+        background: "#EAE7F3",
+        surface: "#EAE7F3",
+        primary: "#069e70",
+        secondary: "#b2ffdb",
+        error: "#EF3966",
+        info: "#97DFF2",
+        success: "#0F6C48",
+        warning: "#F5AA47"
+    }
+}
+
 // setup vuetify
 const vuetify = createVuetify({
     components,
@@ -26,7 +55,35 @@ const vuetify = createVuetify({
         defaultSet: 'mdi'
     },
     theme: {
-        defaultTheme: 'dark'
+        defaultTheme: 'customDarkTheme',
+        themes: {
+            customDarkTheme: {
+                dark: true,
+                colors: {
+                    background: "#31393F",
+                    surface: "#31393F",
+                    primary: "#a00b35",
+                    secondary: "#92f986",
+                    error: "#E33544",
+                    info: "#3D85D6",
+                    success: "#61EAC8",
+                    warning: "#F2C26E"
+                }
+            },
+            customLightTheme: {
+                dark: false,
+                colors: {
+                    background: "#EAE7F3",
+                    surface: "#EAE7F3",
+                    primary: "#069e70",
+                    secondary: "#b2ffdb",
+                    error: "#EF3966",
+                    info: "#97DFF2",
+                    success: "#0F6C48",
+                    warning: "#F5AA47"
+                }
+            }
+        }
     }
 });
 
